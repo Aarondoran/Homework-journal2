ce988d2d143f485955b7479dcf66c7b02a4efa97
Aaron doran <aarondoran@outlook.ie>
2026-09-09 17:09:42 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
     apiKey: "AIzaSyC2xTVoE15-EkyWHWRxIg1TCEIXjyXS7ss",
     authDomain: "homework-journal2.firebaseapp.com",
     projectId: "homework-journal2",
-    storageBucket: "homework-journal2.firebasestorage.app",
+    storageBucket: "homework-journal2.appspot.com",
     messagingSenderId: "538863510993",
     appId: "1:538863510993:web:6dbf7faf76933488282cf0"
   };
